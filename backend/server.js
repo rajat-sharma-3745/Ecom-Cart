503a51417182418655f48ebd586df9ea36789b39
Rajat <sharmarajat3745@gmail.com>
2025-10-28 19:17:33 +0530

    mounted routers at specified paths

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDb } from './config/db.js';
+import productRoutes from './routes/productRoutes.js'
+import cartRoutes from './routes/cartRoutes.js'
+import checkoutRoutes from './routes/checkoutRoutes.js'
 
 dotenv.config();
 
@@ -12,6 +15,10 @@ app.use(cors())
 
 const PORT = process.env.PORT || 5000;
 
+
+app.use('/api/products',productRoutes);
+app.use('/api/cart',cartRoutes);
+app.use('/api/checkout',checkoutRoutes);
 
 
 app.get('/', (req, res) => {
